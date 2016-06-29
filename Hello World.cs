a72185641215d10e88ad4f27a60b7bbf4931a9c9
Zcarie <zaca_13@hotmail.com>
2016-06-28 18:53:57 -0700

    This is the Hello World project

---
+++ Hello World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main()
+        {
+            //this is my first project
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
